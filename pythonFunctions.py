3fa21010c85e7b30e3dc919274504dd49e535f7f
Titch <westr7474@gmail.com>
2022-09-20 12:26:05 +0100

    Created the first function

---
+++ pythonFunctions.py
@@ -0,0 +1,9 @@
+# Functions are code blocks that only run when called
+# Use the def keyword to create a function
+
+# Creating a function
+def firstFunction():
+    print("Hello world from function")
+
+# Calling a function
+firstFunction()
